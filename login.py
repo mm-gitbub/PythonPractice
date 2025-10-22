e13f627f7f2f7900bed483932713639ff7048c56
Madhusmita Mohapatra <madhusmita.mohapatra2416@gmail.com>
2025-10-22 12:58:51 -0400

    First_Commit

--- login.py
+++ login.py
@@ -4,4 +4,4 @@ def login(user_name, password):
         return True
     else:
         return False
-assert login("user", "patsswd") == True, "Login is failed!"
+assert login(  "user", "passwd") == True, "Login is failed!"
