45f06dd8d89b188a7aae76dc135e3c634eae4b3d
Madhusmita Mohapatra <madhusmita.mohapatra2416@gmail.com>
2025-10-22 13:17:00 -0400

    function_testing_Commit

---
+++ calc_test.py
@@ -0,0 +1,11 @@
+def test_sum():
+    sum = 2 + 3
+    assert sum == 5
+
+def test2_sum():
+    sum = 2 + 5
+    assert sum == 8
+
+def test_multiply():
+    multipy = 2 * 2
+    assert multipy == 4
