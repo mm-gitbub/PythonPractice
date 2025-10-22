a922421622b09346e2c67bfb6b296617896af06b
Madhusmita Mohapatra <madhusmita.mohapatra2416@gmail.com>
2025-10-22 14:58:55 -0400

    calculation_testing_Commit

---
+++ reversed_string.py
@@ -0,0 +1,6 @@
+#Program to reversed a string
+#the original string is Hello
+original_str = "Hello"
+#Reversing the string using the built in function reversed, then join the characters using join jountion
+reversed_str = ''.join(reversed(original_str))
+print(reversed_str)
